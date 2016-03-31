ffa9be2448584a99700faeae7ac234bd7bd56e70
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-03-31 14:29:18 -0500

    removed fill copy

--- scripts.js
+++ scripts.js
@@ -3,17 +3,17 @@ window.onload = function() {
   { "title": "Cats on Code",
     "url": "http://catsoncode.com",
     "img": "imgs/cats_on_code_landing.png",
-    "desc": "My personal blog, made with jekyll with a custom layout and hosted on github pages. "
+    "desc": ""
     },
     { "title": "Humble Hearts",
     "url": "http://www.humbleheartsfoundation.org/",
     "img": "imgs/humble_hearts.png",
-    "desc": "Collaborated with a team of 9, during a 24 hour hackathon to create a website for a local charity. We were honored with Humble Hearts."
+    "desc": ""
     },
     { "title": "Raft",
     "url": "http://raft.herokuapp.com",
     "img": "imgs/raft_landing.png",
-    "desc": "Ten days, from ideation to creation, with a team of 3, to create this final project application for DevBootcamp. I designed and implemnted the frontend with Mapbox API. Also designed and setup the database."
+    "desc": ""
     }
   ]
 
