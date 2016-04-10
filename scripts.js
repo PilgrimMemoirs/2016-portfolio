eba51de4ae6dbbb2259da6478368badd6412fe44
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-04-09 23:21:05 -0500

    added cname

--- scripts.js
+++ scripts.js
@@ -2,16 +2,19 @@ window.onload = function() {
   var portfolio = [
   { "title": "Cats on Code",
     "url": "http://catsoncode.com",
+    "github": "https://github.com/PilgrimMemoirs/pilgrimmemoirs.github.io",
     "img": "imgs/cats_on_code_landing.png",
     "desc": ""
     },
     { "title": "Humble Hearts",
     "url": "http://www.humbleheartsfoundation.org/",
+    "github": "https://github.com/wiliajc87/humblehearts",
     "img": "imgs/humble_hearts.png",
     "desc": ""
     },
     { "title": "Raft",
     "url": "http://raft.herokuapp.com",
+    "github": "https://github.com/PilgrimMemoirs/DBC_Raft",
     "img": "imgs/raft_landing.png",
     "desc": ""
     }
@@ -24,7 +27,7 @@ window.onload = function() {
       $( this ).css( "background-image", "url('" + portfolio[counter]["img"] + "')" ).fadeIn(1999);
       $("#portfolio_url").text( portfolio[counter]["title"]);
       $("#portfolio_url").attr( "href", portfolio[counter]["url"] );
-      $("#portfolio_desc").text(portfolio[counter]["desc"]);
+      $("#github_link").attr( "href", portfolio[counter]["github"]);
 
       if (counter <= 1){
         counter ++;
