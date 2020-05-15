ac3753a38fb8dcb9a9be9a521fb986faa91fdcbc
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2020-05-14 20:31:02 -0700

    basic mobile still wip, added projects

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
 $( document ).ready(function() {
 
     $( function() {
-      $( "#about, #projects, #connect" ).draggable();
+      $( "#about, #projects, #connect" ).draggable().resizable();
     });
 
     $( "div[id$=icon]" ).on("click", function() {
       $("#" + this.id.split("-")[0]).show();
+      $("#" + this.id.split("-")[0]).css({"top":"-250px", "left":"200px"});
     });
 
   $( "p[id^=exit]" ).on("click", function() {
     $("#" + this.id.split("-")[1]).hide();
   });
-
-
 });
